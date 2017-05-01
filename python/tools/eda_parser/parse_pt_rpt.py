d584c3a99b7c622cf7a27201f737c479c51895f9
yaguo <viguo@live.cn>
2017-05-01 05:14:15 -0400

    update plot and finished icc_qor report

--- python/tools/eda_parser/parse_pt_rpt.py
+++ python/tools/eda_parser/parse_pt_rpt.py
@@ -6,7 +6,7 @@ import re
 import gzip
 import pyparsing as pp
 
-class parse_pt_sort_rpt():
+class ptRpt():
     '''
         self.pt_rpt_file = pt_rpt_file
         self.rpt_list = []
@@ -113,7 +113,8 @@ class parse_pt_sort_rpt():
         sigma_pat       = pp.Group("Sigma" + COLON +  sigma )
         column_pat      = pp.Group("Point"   + pp.OneOrMore(path_column))
         clk_pat         = pp.Group("clock"+ clk_name + LPAR + toggle + "edge" + RPAR + trans + delay + arrive)
-        lch_clk_pin_pat = pp.Group(pin_name + LPAR + lib_cell_name + RPAR + delay + annotate + arrive)
+        lch_clk_pin_pat = pp.Group(pin_name + LPAR + lib_cell_name + RPAR + delay + annotate + arrive + toggle)
+        input_delay     = pp.Group("input external delay" + delay + arrive +toggle)
 
         flat_pin_pat    = pp.Group(pin_name+ LPAR + lib_cell_name + RPAR + trans + delay + pp.ZeroOrMore(annotate) + arrive + toggle)
         flat_net_pat    = pp.Group(net_name + LPAR + "net" + RPAR + fanout + cap)
@@ -121,6 +122,7 @@ class parse_pt_sort_rpt():
         hier_pin_pat    = pp.Group(pin_name + LPAR + module_name + RPAR + trans_zero +  delay_zero + pp.ZeroOrMore(annotate)+ arrive + toggle)
         hier_net_pat    = pp.Group(net_name + LPAR + "net" + RPAR)
 
+        flat_io_pat     = pp.Group(pin_name+ LPAR + pp.oneOf("in net out ") + RPAR + trans + delay + pp.ZeroOrMore(annotate) + arrive + toggle)
         clk_uncer_pat   = pp.Group("clock uncertainty" + uncertainty + arrive)
         clk_nw_pat      = pp.Group("clock network delay" + LPAR + clk_type + RPAR +  delay + arrive )
         crpr_pat        = pp.Group("clock reconvergence pessimism" + delay + arrive)
@@ -130,36 +132,41 @@ class parse_pt_sort_rpt():
         data_arr_pat    = pp.Group("data arrival time" + arrive)
         stat_adj_pat    = pp.Group("statistical adjustment" + delay + arrive)
         slack_pat       = pp.Group("slack" + LPAR +"VIOLATED" + RPAR + slack  )
+        unconst_path    = pp.Group(LPAR + "Path is unconstrained" + RPAR)
 
         #data_path_pat = pp.Group(pp.OneOrMore(flat_pin_pat)+pp.ZeroOrMore(flat_net_pat)+ pp.ZeroOrMore(hier_net_pat)+pp.ZeroOrMore(hier_pin_pat))
-        data_path_pat = pp.Group(pp.OneOrMore(flat_pin_pat | flat_net_pat | hier_net_pat |hier_pin_pat))
+        data_path_pat = pp.Group(pp.OneOrMore(flat_pin_pat | flat_net_pat |flat_io_pat | hier_net_pat |hier_pin_pat))
+
+        #pt_path_pat = pp.Group(
+        #    sp_pat +
+        #    ep_pat +
+        #    path_group_pat +
+        #    path_type_pat +
+        #    derate_pat +
+        #    sigma_pat +
+        #    column_pat +
+        #    carve +
+        #    input_delay +
+        #    data_path_pat +
+        #    data_arr_pat +
+        #    carve +
+        #    unconst_path
+        #)
 
         pt_path_pat = pp.Group(
             sp_pat +
             ep_pat +
-            path_group_pat +
+            path_group_pat+
             path_type_pat +
             derate_pat +
             sigma_pat +
             column_pat +
             carve +
-            clk_pat +
-            clk_nw_pat +
-            data_path_pat +
-            data_arr_pat +
-            clk_pat +
-            clk_nw_pat +
-            crpr_pat +
-            clk_uncer_pat +
-            cap_clk_pin_pat +
-            lib_setup_pat +
-            data_req_pat +
-            carve +
-            data_req_pat +
+            input_delay +
+            data_path_pat+
             data_arr_pat +
             carve +
-            stat_adj_pat +
-            slack_pat
+            unconst_path
         )
         rpt_file = self.pt_rpt_file
         print rpt_file
@@ -167,68 +174,81 @@ class parse_pt_sort_rpt():
             if rpt_file.split(".")[-1] == "gz":
                 gzipfile = gzip.open(rpt_file,'r')
                 rpt_string  = gzipfile.read()
-                #print  len(rpt_string)
-                #result = sp_pat.searchString(rpt_string)
-                #result = ep_pat.searchString(rpt_string)
-                #result = path_group_pat.searchString(rpt_string)
-                #result = path_type_pat.searchString(rpt_string)
-                #result = derate_pat.searchString(rpt_string)
-                #result = sigma_pat.searchString(rpt_string)
-                #rs = column_pat.searchString(rpt_string)
-                #rs = clk_pat.searchString(rpt_string)
-                #rs = lch_clk_pin_pat.searchString(rpt_string)
-                #rs = flat_pin_pat.searchString(rpt_string)
-                #rs = flat_net_pat.searchString(rpt_string)
-                #rs = hier_pin_pat.searchString(rpt_string)
-                #rs = hier_net_pat.searchString(rpt_string)
+            else:
+                file  = open(rpt_file,"r")
+                rpt_string = file.read()
 
-                #rs = clk_uncer_pat.searchString(rpt_string)
-                #rs = clk_nw_pat.searchString(rpt_string)
-                #rs = crpr_pat.searchString(rpt_string)
-                #rs = cap_clk_pin_pat.searchString(rpt_string)
-                #rs= lib_setup_pat.searchString(rpt_string)
-                #rs = data_req_pat.searchString(rpt_string)
-                #rs = data_arr_pat.searchString(rpt_string)
-                #rs = stat_adj_pat.searchString(rpt_string)
-                #rs = slack_pat.searchString(rpt_string)
-                #rs = data_path_pat.searchString(rpt_string)
-                #pt_rpt_db = pt_path_pat.searchString(rpt_string)
-                self.rpt_list = pt_path_pat.searchString(rpt_string)
-                #rs = carve.searchString(rpt_string)
-                #for i in range(0,len(rs[0][0])):
-                #   print rs[0][0][i]
-                #print len(rs[0])
-                #print rs[1]
-                #for i in range(0,len(result)):
-                #    print sp_result[i][0][2]
-                #print len(pt_rpt_db), len(pt_rpt_db[0]),len(pt_rpt_db[0][0])
-                self.length =  len(self.rpt_list)
-                for i in range(0, self.length):
-                    self.startpoint.append(self.rpt_list[i][0][0][2])
-                    self.endpoint.append(self.rpt_list[i][0][1][2])
-                    self.clock.append(self.rpt_list[i][0][2][2])
-                    self.path_type.append(self.rpt_list[i][0][3])
-                    self.max_derate_factor.append(self.rpt_list[i][0][4][3])
-                    self.sigma.append(self.rpt_list[i][0][5][2])
-                    self.column.append(self.rpt_list[i][0][6])
-                    self.launch_clock.append(self.rpt_list[i][0][8][1])
-                    self.clk_network.append(self.rpt_list[i][0][9][1])
-                    self.arrive_time.append(self.rpt_list[i][0][11][1])
-                    self.capture_clock.append(self.rpt_list[i][0][12][1])
-                    self.crpr.append(self.rpt_list[i][0][14][1])
-                    self.uncertainty.append(self.rpt_list[i][0][15][1])
-                    self.cap_clk_pin.append(self.rpt_list[i][0][16][0])
-                    self.lib_setup.append(self.rpt_list[i][0][17][1])
-                    self.data_req_time.append(self.rpt_list[i][0][18][1])
-                    self.stat_adj.append(self.rpt_list[i][0][23][1])
-                    self.slack.append(self.rpt_list[i][0][2])
+            #print  len(rpt_string)
+            #result = sp_pat.searchString(rpt_string)
+            #result = ep_pat.searchString(rpt_string)
+            #result = path_group_pat.searchString(rpt_string)
+            #result = path_type_pat.searchString(rpt_string)
+
+            #result = derate_pat.searchString(rpt_string)
+            #result = sigma_pat.searchString(rpt_string)
+            #rs = column_pat.searchString(rpt_string)
+            #rs = clk_pat.searchString(rpt_string)
+            #rs = lch_clk_pin_pat.searchString(rpt_string)
+            #rs = flat_pin_pat.searchString(rpt_string)
+            #rs = flat_net_pat.searchString(rpt_string)
+            #rs = hier_pin_pat.searchString(rpt_string)
+            #rs = hier_net_pat.searchString(rpt_string)
+
+            #rs = clk_uncer_pat.searchString(rpt_string)
+            #rs = clk_nw_pat.searchString(rpt_string)
+            #rs = crpr_pat.searchString(rpt_string)
+            #rs = cap_clk_pin_pat.searchString(rpt_string)
+            #rs= lib_setup_pat.searchString(rpt_string)
+            #rs = data_req_pat.searchString(rpt_string)
+            #rs = data_arr_pat.searchString(rpt_string)
+            #rs = stat_adj_pat.searchString(rpt_string)
+            #rs = slack_pat.searchString(rpt_string)
+            #rs = data_path_pat.searchString(rpt_string)
+            #print rs[0][0][-1]
+            #return
+            #print "aa"
+            #pt_rpt_db = pt_path_pat.searchString(rpt_string)
+            self.rpt_list = pt_path_pat.searchString(rpt_string)
+            #print      self.rpt_list
+            #return
+            #rs = carve.searchString(rpt_string)
+            #for i in range(0,len(rs[0][0])):
+            #   print rs[0][0][i]
+            #print len(rs[0])
+            #print rs[1]
+            #for i in range(0,len(result)):
+            #    print sp_result[i][0][2]
+            #print len(pt_rpt_db), len(pt_rpt_db[0]),len(pt_rpt_db[0][0])
+            self.length =  len(self.rpt_list)
+            for i in range(0, self.length):
+                #print self.rpt_list[i][0][-3][-1]
+                self.startpoint.append(self.rpt_list[i][0][0][2])
+                self.endpoint.append(self.rpt_list[i][0][1][2])
+                self.arrive_time.append(self.rpt_list[i][0][-3][-1])
+
+                #self.clock.append(self.rpt_list[i][0][2][2])
+                #self.path_type.append(self.rpt_list[i][0][3])
+                #self.max_derate_factor.append(self.rpt_list[i][0][4][3])
+                #self.sigma.append(self.rpt_list[i][0][5][2])
+                #self.column.append(self.rpt_list[i][0][6])
+                #self.launch_clock.append(self.rpt_list[i][0][8][1])
+                #self.clk_network.append(self.rpt_list[i][0][9][1])
+                #self.arrive_time.append(self.rpt_list[i][0][11][1])
+                #self.capture_clock.append(self.rpt_list[i][0][12][1])
+                #self.crpr.append(self.rpt_list[i][0][14][1])
+                #self.uncertainty.append(self.rpt_list[i][0][15][1])
+                #self.cap_clk_pin.append(self.rpt_list[i][0][16][0])
+                #self.lib_setup.append(self.rpt_list[i][0][17][1])
+                #self.data_req_time.append(self.rpt_list[i][0][18][1])
+                #self.stat_adj.append(self.rpt_list[i][0][23][1])
+                #self.slack.append(self.rpt_list[i][0][2])
 
 
 
 
-                    #print self.rpt_list[i][0][24]
+                #print self.rpt_list[i][0][24]
 
-                return self.rpt_list
+            return self.rpt_list
 
 
 
@@ -401,40 +421,80 @@ class parse_pt_sort_rpt():
                 return self.rpt_list
 
 
-class parse_icc_qor:
+class iccQor:
     def __init__(self, qor_file):
         self.qor_file = qor_file
+        self.qorAll = []
         print "parsing" , self.qor_file
 
-    def read_icc_qor:
-        COLON, LBRACK, RBRACK, LBRACE, RBRACE, TILDE, CARAT,COMMA = map(pp.Literal, ":[]{}~^,")
-        UNDERSCORE, DOT, BSLASH, SLASH = map(pp.Literal, '_./\\')
-        PLUS, DASH = map(pp.Suppress, "+-")
+    def read_icc_qor(self):
+        LBRACK, RBRACK, LBRACE, RBRACE, TILDE, CARAT,COMMA = map(pp.Literal, "[]{}~^,")
+        UNDERSCORE,  BSLASH, SLASH = map(pp.Literal, '_/\\')
+        PLUS, DASH ,COLON,DOT = map(pp.Suppress, "+-:.")
         LPAR, RPAR = map(pp.Suppress, "()")
         SEMICOLON, DQUOTA = map(pp.Suppress, ';"')
         SQUOTA,DQUOTA = map(pp.Suppress, "\'\"")
+        STAR = map(pp.Literal,'*')
 
-        float = pp.Word(pp.nums + '.' + "_")
+
+        float = pp.Word(pp.nums + '.' + "_" + '-')
         integer = pp.Word(pp.nums + '-')
-        flatName = pp.Word(pp.alphanums + "_"+"-")
+        flatName = pp.Word(pp.alphanums + "_"+"-" + '.')
         hierName = pp.Word(pp.alphanums + '/' + '_' + '[' + ']')
-        pathGroupName = pp.Word(pp.alphanums+'*' + '_')
+        pathGroupName = pp.Word(pp.alphanums+'\*' + '_' + "'")
+        scenarioName = pp.Word(pp.alphanums + "_" + "'")
+        date = pp.Word(pp.alphanums + " " + ":")
+
+        sQuoteString = pp.QuotedString("'")
+        dQuoteString = pp.QuotedString('"')
+        starQuoteString = pp.QuotedString('*')
+        eqQuoteString = pp.QuotedString('=')
+        paraQuoteSting = pp.QuotedString('(', endQuoteChar=')')
+        tildeQuoteString = pp.QuotedString('~')
+        angleQuoteString = pp.QuotedString('<', endQuoteChar='>')
+
 
         dashLine = pp.Suppress(pp.Group(pp.OneOrMore(DASH)))
+        starLine = pp.Suppress(pp.Group(pp.OneOrMore('*')))
+        designName = pp.Group("Design" + COLON + flatName)
 
-        design = pp.Group("Design" + flatName)
-        version = pp.Group("Version" + flatName)
-        sceTimGroup = pp.Group("Scenario" + SQUOTA + flatName + SQUOTA +
-                                 "Timing Path Group" + SQUOTA + pathGroupName + SQUOTA +
-                                 pp.OneOrMore(DASH) +
-                                 "Levels of Logic" + COLON + integer +
-                                 "Critical Path Length" + COLON + float +
-                                 "Critical Path Slack" + COLON + float +
-                                 "Critical Path Clk Period" + COLON + float +
-                                 "Total Negative Slack" + COLON + float +
-                                 "No. of Violating Paths" + COLON + float +
-                                 pp.OneOrMore(DASH)
-                                 )
+        #qorHeader = pp.Group("Report" + COLON + flatName +
+        #                    "Design" + COLON + flatName +
+        #                    "Version" + COLON + flatName +
+        #                    "Date" + COLON + date +
+        #                    starLine +
+        #                    "Information: Timer using" + pp.delimitedList(sQuoteString) + DOT  + pp.delimitedList(paraQuoteSting)
+        #                     )
+        qorHeader = pp.Group("Report" + COLON + flatName +
+                            "Design" + COLON + flatName +
+                            "Version" + COLON + flatName +
+                            "Date" + COLON + date +
+                            starLine +
+                            "Information: Timer using" + pp.delimitedList(sQuoteString) + DOT  + pp.delimitedList(paraQuoteSting)
+                             )
+
+        designName = pp.Group("Design" + COLON + flatName)
+        iccVersion = pp.Group("Version" + COLON + flatName)
+        writeDate  = pp.Group("Date" + COLON + date)
+        timer  = pp.Group("Information" + COLON + "Timer using" + pp.delimitedList(sQuoteString) + DOT  + pp.delimitedList(paraQuoteSting) )
+        #timer  = pp.Group(pp.delimitedList(paraQuoteSting))
+        #timer = pp.Group(pp.delimitedList(sQuoteString))
+        #timerType = pp.Group("Design" + COLON + flatName)
+
+
+
+        sceTimGroup = pp.Group("Scenario" +pp.delimitedList(sQuoteString) +
+                               "Timing Path Group" + pp.delimitedList(sQuoteString) +
+                               dashLine +
+                               "Levels of Logic" + COLON + integer +
+                                "Critical Path Length" + COLON + float +
+                                "Critical Path Slack" + COLON + float +
+                                "Critical Path Clk Period" + COLON + float +
+                                "Total Negative Slack" + COLON + float +
+                                "No. of Violating Paths" + COLON + integer +
+                                dashLine
+                               )
+
         cellCount = pp.Group(
             "Cell Count" + dashLine +
             "Hierarchical Cell Count:" +integer +
@@ -447,29 +507,73 @@ class parse_icc_qor:
             "Combinational Cell Count:" + integer +
             "Sequential Cell Count:" + integer +
             "Macro Count:" + integer +
-            pp.OneOrMore(DASH)
+            dashLine
         )
 
-        area = pp.Group("Area" + dashLine +
+        cellArea = pp.Group("Area" + dashLine +
             "Combinational Area:" + float +
             "Noncombinational Area:" + float +
-            "Buf/Inv Area:" + float +
+            'Buf/Inv Area:' + float +
             "Total Buffer Area:" + float +
             "Total Inverter Area:" + float +
-            "Macro/Black Box Area:" + float +
+            'Macro/Black Box Area:' + float +
             "Net Area:" + float +
             "Net XLength:" + float +
-            "Net YLength:" + float + dashLine +
-            "Cell Area (netlist):" + float +
-            "Cell Area (netlist and physical only):        70319.33"     + float +
+            "Net YLength:" + float +
+            dashLine +
+            'Cell Area (netlist):' + float +
+            'Cell Area (netlist and physical only):'     + float +
             "Net Length:" + float
         )
 
-        designRule =pp.Group("Design Rules" + dashLine +
+        designRule =pp.Group("Design Rules" +
+                             dashLine +
                              "Total Number of Nets:" + integer +
                              "Nets with Violations:" + integer +
                              "Max Trans Violations:" + integer +
-                             "Max Cap Violations:" + integer +dashLine)
+                             "Max Cap Violations:" + integer +
+                             dashLine)
 
+
+        qorAll = pp.Group(qorHeader +
+                         pp.OneOrMore(sceTimGroup) +
+                         cellCount +
+                         cellArea +
+                         designRule)
+
+        rpt_file = self.qor_file
+        # print rpt_file
+        if os.path.isfile(rpt_file):
+            if rpt_file.split(".")[-1] == "gz":
+                file = gzip.open(rpt_file, 'r')
+            else:
+                file = open(rpt_file,"r")
+            rpt_string = file.read()
+            #rst = designName.searchString(rpt_string)
+            #rst = iccVersion.searchString(rpt_string)
+            #rst = writeDate.searchString(rpt_string)
+            #rst =  cellCount.searchString(rpt_string)
+            #rst = cellArea.searchString(rpt_string)
+            #rst = designRule.searchString(rpt_string)
+            #print rpt_string
+            #rst = designName.searchString(rpt_string)
+            #print rst
+            #rst = sceTimGroup.searchString(rpt_string)                                                                                                          9
+            #rst =qorHeader.searchString(rpt_string)
+            #rst = timer.searchString(rpt_string)
+            self.qorAll = qorAll.searchString(rpt_string)
+            #print rst
+            #for i in range(0,len(rst)):
+            #    print rst[i]
+
+    def criticalData(self):
+        timeData = []
+        timeData.append(self.qorAll[0][0][3][1])
+        timeData.append(self.qorAll[0][0][3][3])
+        timeData.append(self.qorAll[0][0][3][9])
+        timeData.append(self.qorAll[0][0][3][11])
+        timeData.append(self.qorAll[0][0][3][13])
+        #timeData.append(self.qorAll[0][0][3])
+        return timeData
 
 
--- python/tools/eda_parser/parser_regression.py
+++ python/tools/eda_parser/parser_regression.py
@@ -7,7 +7,10 @@ import re
 import gzip
 import datetime
 import parse_def as p_def
-import parse_pt_rpt as p_pt_rpt
+import parse_pt_rpt as rpt
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import norm
 
 
 
@@ -19,6 +22,59 @@ def main():
     #pt_rpt = p_pt_rpt.parse_pt_rpt(rpt_file)
     #print "end  time", datetime.datetime.now()
 
+    ##regression pt report
+    rpt_file = "fullchip.df_tcdx_t14.df_cs_umc_t8.sorted.gz"
+    rpt_file = "fullchip.umc_umcch_t100.df_cs_umc_t8.sorted.gz"
+    rpt_file = "/home/yaguo/ariel/df_tcdxd_t_16.ft.tt0p9v.rpt"
+    #rpt_file = "/proj/ariel_pd_vol38/yaguo/df_tcdx_t16/main/pd/tiles/df_tcdxd_t_16_r1_buf_test3_1_TileBuilder_Apr10_2238_13535_GUI/df_tcdxd_t_16.ft.tt0p9v.rpt.small"
+    #print "start time", datetime.datetime.now()
+    p_pt_rpt = rpt.ptRpt(rpt_file)
+    p_pt_rpt.read_ft_uncons()
+    #print "end  time", datetime.datetime.now()
+    #print p_pt_rpt.startpoint[0], p_pt_rpt.endpoint[0] , p_pt_rpt.arrive_time[0]
+
+
+    delay_list =   np.array(p_pt_rpt.arrive_time).astype(np.float)
+    #delay_list =   p_pt_rpt.arrive_time
+    sum_delay = np.sum(delay_list)
+    print delay_list.sum(),delay_list.max(),delay_list.min(),delay_list.mean()
+    print delay_list
+
+    bins = [delay_list.min(),delay_list.max()]
+    uvals = np.unique(delay_list+[10,])
+    hist ,bins  = np.histogram(delay_list, bins=uvals)
+
+    gaussian_numbers = np.asarray(delay_list)
+    #plt.hist(gaussian_numbers)
+    delay_avg = float("{0:.2f}".format(delay_list.mean()))
+    delay_max = float("{0:.2f}".format(delay_list.max()))
+    delay_min = float("{0:.2f}".format(delay_list.min()))
+    xlabel = "Delay(avg="+ str(delay_avg)+"max=" + str(delay_max) + "min=" + str(delay_min)+")"
+    ylable = "Count(" + str(len(delay_list))+ ")"
+    plt.title("df_tcdxd_t16")
+    plt.xlabel(xlabel)
+
+    plt.ylabel(ylable)
+    #fig = plt.gcf()
+    #plot_url = py.plot_mpl(fig, filename='mpl-basic-histogram')
+    #plt.plot(delay_list,norm.pdf(delay_list,0.4))
+    #width = 0.7*(bins[1]-bins[0])
+    #center = delay_list.mean()
+    #plt.bar(center, hist, align = 'center', width = width)
+    plt.bar(bins[:-1],hist,width=np.diff(bins))
+    plt.show()
+
+
+    #xbins=range(0,len(delay_list))
+    #plt.hist(delay_list,bins=xbins)
+    #plt.title("Gaussian Histogram")
+    #plt.xlabel("Value")
+    #plt.ylabel("Frequency")
+    #plt.show()
+
+    #fig = plt.gcf()
+
+    #plot_url = py.plot_mpl(fig, filename='mpl-basic-histogram')
     #regression DEF parsing
     #def_file = r'D:/AMD/parser_case/Place.def'
     #def_fp = p_def.parse_def(def_file)
@@ -27,8 +83,26 @@ def main():
     #print "start time", datetime.datetime.now()
 
     #regression icc qor parsing
-    rpt_file =  "/proj/ariel_pd_vol38/yaguo/df_tcdx_t16/main/pd/tiles/df_tcdxd_t_16_r1_buf_org3_TileBuilder_Apr10_2238_13535_GUI/rpts/I2Place/qor.rpt.gz"
-    icc_qor  =  p_pt_rpt.parse_icc_qor(rpt_file)
+
+    #basRunDir = "/proj/ariel_pd_vol38/yaguo/df_tcdx_t16/main/pd/tiles/"
+    #basNickName = "r1_buf_org3"
+    #
+    #refRunDir = "/proj/ariel_pd_vol38/yaguo/df_tcdx_t16/main/pd/tiles/"
+    #refNickNmae = "r1_buf_test3_1"
+    #
+    #stageRpt = "/rpts/I2ReRoute/qor.rpt.gz"
+    #
+    #basRptFile =  basRunDir + basNickName + stageRpt
+    #refRptFile =refRunDir + refNickNmae + stageRpt
+    #
+    #basIccQor  =  rpt.iccQor(basRptFile)
+    #basIccQor.read_icc_qor()
+    #
+    #refIccQor   =  rpt.iccQor(refRptFile)
+    #refIccQor.read_icc_qor()
+    #
+    #print basNickName,basIccQor.criticalData()
+    #print refNickNmae,refIccQor.criticalData()
 
 
 if __name__ == '__main__':
