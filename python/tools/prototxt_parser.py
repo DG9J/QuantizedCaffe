f3abe7bd37bb0dbbaeaac7eb10b94487533975d6
viguo <viguo@live.cn>
2017-05-29 00:35:32 +0800

    udpate the prototxt_parser

--- python/tools/prototxt_parser.py
+++ python/tools/prototxt_parser.py
@@ -13,8 +13,36 @@ layer_dict => [n] =>
 
 import argparse
 import csv
-from pyparsing import *
+import pyparsing as pp
+import icComVar as icVar
+import itertools
 
+def pattern_match(pattern,target_string):
+    #print "pattern:", pattern
+    result = pattern.searchString(target_string).asList()
+    #print "result:", result
+    return result
+
+def list_flatten(l, a=None):
+    #check a
+    if a is None:
+        #initialize with empty list
+        a = []
+    for i in l:
+        if isinstance(i, list):
+            list_flatten(i, a)
+        else:
+            a.append(i)
+    return a
+def get_values(lVals):
+    for val in lVals:
+        if isinstance(val, list):
+            get_values(val)
+        else:
+            return val
+
+
+'''
 
 parser = argparse.ArgumentParser()
 # Required arguments: input and output files.
@@ -37,69 +65,67 @@ def flatten(the_list,new_list):
 
 with open(args.proto_file) as f:
     s = f.read()
+'''
+file = "./example/ResNet-50-deploy.prototxt"
+with open(file) as f:
+    s = f.read()
+    s = s.replace('"',"")
+    s = s.replace(":","")
 
-LBRACE,RBRACE,LBRACK,RBRACK,COMMA,COLON,DOT = map(Suppress,"{}[],:.")
+flatName    = pp.Word(pp.alphanums + "_"+":")
+attrDefine = pp.Group(flatName   + flatName )
+paraAttr = pp.Group(pp.OneOrMore(flatName + flatName))
+paraDefine = pp.Group(flatName + icVar.LBRACE + paraAttr + icVar.RBRACE)
+layerAttr = pp.Group(pp.OneOrMore(attrDefine|paraDefine))
+layerDefine = pp.Group("layer" + icVar.LBRACE + layerAttr  + icVar.RBRACE)
+protoDefine = pp.Group(pp.OneOrMore(attrDefine) + pp.OneOrMore(layerDefine))
+#print type(s),s
+result = pattern_match(protoDefine,s)[0][0]
+#print type(result), len(result),result
+j = 0
+for i in range(0,len(result)):
+    if result[i][0] == "layer":
+        result[i][0] = j
+        j += 1
+    else:
+        result[i][0] = result[i][0].replace(":", "").replace('"','')
+        if result[i][0] == "input_dim":
+            result[i][0] += str(j)
+            j += 1
 
-key    = Word(alphanums+"_")
-value  = Word(alphanums+"_")
-attr_format  = Group(key +  COLON + Suppress(ZeroOrMore('"'))+ value + Suppress(ZeroOrMore('"')))
-shape_format = Suppress("shape") + COLON + LBRACE + OneOrMore(attr_format) + RBRACE
-para_format = Group(Suppress(key) + LBRACE  + ZeroOrMore(attr_format) + ZeroOrMore(shape_format) + RBRACE)
-layer_format = Suppress("layer") + LBRACE + OneOrMore(attr_format) + ZeroOrMore(para_format) + RBRACE
+#print len(result),result
+protoDict= dict(result)
+j = 0
+for key, value in protoDict.iteritems():
+    if isinstance(key,str):
+        print "attr",key, value
+    elif isinstance(key,int):
+        if isinstance(value,list):
+            protoDict[key] = dict(value)
+            j += 1
+            for l_key , l_value in protoDict[key].iteritems():
+                l_key = l_key.replace(":", "")
+                if isinstance(l_value,list):
+                    #print dict(l_value)
+                    protoDict[key][l_key] = dict(itertools.izip_longest(*[iter(l_value)] * 2, fillvalue=""))
+                    #print key,l_key,protoDict[key][l_key]
+                elif isinstance(l_value,str):
+                    #print "str:",key, l_key, l_value
+                    protoDict[key][l_key]= l_value.replace("\"", "")
+                else:
+                    print "Error:", l_value
+                print key,l_key,protoDict[key][l_key]
+            #for l_attr in value:
+            #    print type(l_attr),len(l_attr), l_attr, dict(l_attr)
+            #protoDict[key] = dict(value)
+            #for layer_key, layer_value in protoDict[key].iteritems():
+            #    print "layer dict", layer_key,layer_value
+    else:
+        print "Error:"
 
-layer_results = layer_format.searchString(s)
-layer_dict = list()
-layer_keys = list()
-layer_names = list()
 
-layer_numb = 0
-for items in layer_results:
-    layer_numb += 1
-    layer_list = list()
-    layer_name = "Unknow"
-    flatten(items.asList(),layer_list)
-    for i in range(0,len(layer_list)):
-        if layer_list[i] == "name":
-            layer_name = layer_list[i+1]
-    if  layer_name not in layer_names:
-        layer_names.append(layer_name)
-    temp_dict = {}
-    for i in range(0,len(layer_list),2):
-        key = layer_list[i]
-        value = layer_list[i+1]
-        if key not in temp_dict:
-            temp_dict[key]=list()
-        temp_dict[key].append(value)
-        if key not in layer_keys:
-            layer_keys.append(key)
-    layer_dict.append(temp_dict)
-layer_keys.insert(1,"layer")
 
-layer_array = list()
-layer_array.append(layer_keys)
-empty_list = [" " for i in range(len(layer_keys))]
-base_line = 1
-for i in range(0,len(layer_dict)):
-    current_line = base_line
-    jump_line = 0
-    max_para = 0
-    temp_dict = layer_dict[i]
-    for t in temp_dict:
-        if len(temp_dict[t]) > max_para :
-            max_para = len(temp_dict[t])
-    for t in range(0,max_para):
-        layer_array.append(empty_list[:])
-    for j in range(0,len(layer_keys)):
-        key = layer_keys[j]
-        if key == "layer":
-            layer_array[current_line][j] = i
-        elif key in temp_dict:
-            for l in range(0,len(temp_dict[key])):
-                next_line = current_line + l
-                layer_array[next_line][j] = temp_dict[key][l]
 
-    base_line += max_para
-
-with open(args.csv_file, "wb") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(layer_array)
+#with open(args.csv_file, "wb") as csvfile:
+#    writer = csv.writer(csvfile)
+#    writer.writerows(layer_array)
